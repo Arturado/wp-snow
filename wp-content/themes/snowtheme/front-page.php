44f44954c958230314becd2c22374585789e06d0
Arturo Vasquez <arturados@gmail.com>
2026-07-08 19:27:28 -0400

    feat: cambio de texto

--- wp-content/themes/snowtheme/front-page.php
+++ wp-content/themes/snowtheme/front-page.php
@@ -249,7 +249,7 @@ get_header();
     <div class="container">
       <div class="section-header">
         <div>
-          <h2 class="section-title">HAN CONFIADO EN SNOW <span class="text-blue">*</span></h2>
+          <h2 class="section-title">ARTISTAS QUE CONFIAN EN SNOW <span class="text-blue">*</span></h2>
         </div>
       </div>
 
